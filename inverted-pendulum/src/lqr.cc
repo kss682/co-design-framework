3f3e3dc9c53298551afbef86d4ca95feddd2a74b
Srinidhi Krishna <kss682@gmail.com>
2026-01-27 15:36:54 +0100

    - add modes to event - visualize the states in notebook

--- inverted-pendulum/src/lqr.cc
+++ inverted-pendulum/src/lqr.cc
@@ -20,3 +20,10 @@ double LQRegulator::control(const pendulum_state_t state, double pos)
 
     return u;
 }
+
+double LQRegulator::control(const pendulum_state_t state, const pendulum_state_t target_state){
+
+    double u = -((state[0] - target_state[0])*K[0] + (state[1] - target_state[1])*K[1] + (state[2] - target_state[2])*K[2] + (state[3] - target_state[3])*K[3]);
+
+    return u;
+}
